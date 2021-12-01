e7c30728da83ec1e1f35b62466cc4827d942158e
suhaask31 <92918926+suhaask31@users.noreply.github.com>
2021-12-01 20:41:50 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+ 
+
+for(var index=0;index<=9;index++){
+  if(marks[index]>=45){
+  console.log(marks[index])
+}
+}
+
+
+
+
+function setup() 
+{
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
